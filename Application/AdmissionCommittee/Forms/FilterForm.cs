47cfbd707b53c99626372721b5e2ed502f184455
Alexander Yasinovskiy <alex2021cyberhuman@gmail.com>
2022-05-10 18:00:54 +0500

    Добавил просмотру абитуриентов немного стиля

---
+++ Application/AdmissionCommittee/Forms/FilterForm.cs
@@ -0,0 +1,19 @@
+using Application.Common.Enums;
+using Application.Shared.Input;
+
+namespace Application.AdmissionCommittee.Forms;
+
+public class FilterForm
+{
+    public long SelectedSpecialityId { get; set; }
+
+    public List<MyInputRadioGroup<long>.ValueRadioItem> SpecialityRadioItems { get; set; } = new();
+
+    public int SelectedYear { get; set; }
+
+    public List<MyInputRadioGroup<int>.ValueRadioItem> YearRadioItems { get; set; } = new();
+
+    public EducationForm SelectedEducationForm { get; set; } = EducationForm.FullTime;
+
+    public List<MyInputRadioGroup<EducationForm>.ValueRadioItem> EducationForms { get; set; } = new();
+}
