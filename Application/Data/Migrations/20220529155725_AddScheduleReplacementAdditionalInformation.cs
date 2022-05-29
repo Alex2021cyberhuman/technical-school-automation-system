587c111702d76d1f427627a8c20cbb3d5b45eb1d
Alexander Yasinovskiy <alex2021cyberhuman@gmail.com>
2022-05-29 20:59:07 +0500

    Добавил миграцию

---
+++ Application/Data/Migrations/20220529155725_AddScheduleReplacementAdditionalInformation.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Application.Data.Migrations
+{
+    public partial class AddScheduleReplacementAdditionalInformation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_class_schedule_replacement_subject_subject_id",
+                table: "class_schedule_replacement");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "subject_id",
+                table: "class_schedule_replacement",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "is_addition",
+                table: "class_schedule_replacement",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "is_cancel",
+                table: "class_schedule_replacement",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_class_schedule_replacement_subject_subject_id",
+                table: "class_schedule_replacement",
+                column: "subject_id",
+                principalTable: "subject",
+                principalColumn: "id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_class_schedule_replacement_subject_subject_id",
+                table: "class_schedule_replacement");
+
+            migrationBuilder.DropColumn(
+                name: "is_addition",
+                table: "class_schedule_replacement");
+
+            migrationBuilder.DropColumn(
+                name: "is_cancel",
+                table: "class_schedule_replacement");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "subject_id",
+                table: "class_schedule_replacement",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_class_schedule_replacement_subject_subject_id",
+                table: "class_schedule_replacement",
+                column: "subject_id",
+                principalTable: "subject",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
